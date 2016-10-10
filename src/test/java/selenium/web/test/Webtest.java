df7ed1d79ba0c97b5b101906e25a45c197fc7340
Nawaphong <Nawaphong@hostjboss>
2016-10-10 15:52:14 +0700

    update test web1

--- src/test/java/selenium/web/test/Webtest.java
+++ src/test/java/selenium/web/test/Webtest.java
@@ -4,7 +4,6 @@ import static org.testng.AssertJUnit.assertEquals;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.AssertJUnit;
 import java.awt.AWTException;
 import java.awt.Robot;
@@ -13,6 +12,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Ignore;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
@@ -29,8 +29,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Webtest {
-	
-	protected static RemoteWebDriver driver;
+	protected static WebDriver driver;
 	protected static WebDriverWait wait;
 	protected static WebElement element = null;
 	protected static JavascriptExecutor jsExecutor;
@@ -45,19 +44,16 @@ public class Webtest {
 		AssertJUnit.assertTrue(!"test02".equals(name));
 	}
 	
-	@BeforeTest
-	public void start() throws MalformedURLException {
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		driver = new RemoteWebDriver(new URL("http://192.168.109.1:4444/wd/hub"), capabilities);
-		driver.manage().window().maximize();
-	}
-	
 	@BeforeClass
 	public void initWebDriver() throws MalformedURLException{
-		
+		String Node = "http://hostjboss:5556/wd/hub";
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
+//		cap.setCapability(FirefoxDriver.PROFILE);
+		cap.setBrowserName("chrome");
+		driver = new RemoteWebDriver(new URL(Node), cap);
 	}	
 	
-	@Test(enabled = true)
+	@Test
 	public void TestA_insert() throws InterruptedException{
 		driver.get("http://hostjboss:8080/MavenPrimefacesWebTest/");
 		driver.findElement(By.id("form:userName")).sendKeys("test01");
@@ -68,7 +64,8 @@ public class Webtest {
 		AssertJUnit.assertNotNull(element);
 	}
 	
-	@Test(enabled = true)
+	@Ignore
+	@Test
 	public void TestB_update() throws InterruptedException, AWTException{
 		Thread.sleep(2000);
 		element = driver.findElement(By.id("form:userName"));
@@ -89,12 +86,12 @@ public class Webtest {
 		assertTestUpdate(name, login, pass);		
 	}
 
-	@Test(enabled = true)
+	@Test
 	public void TestC_delete() throws InterruptedException, AWTException{
 		Thread.sleep(500);
 		element = getWebDriverWait().until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("form:j_idt15"))));
 		element.click();
-		Robot rb = new Robot();
+		Robot rb =new Robot();
 		rb.keyPress(KeyEvent.VK_ENTER);
 		Thread.sleep(3000);
 		element = getWebDriverWait().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='form:tableUser_data']/tr[1]")));
@@ -103,8 +100,8 @@ public class Webtest {
 	}
 	
 	@AfterTest
-	public void endTest(){
-		driver.close();
+	public void afterTest(){
+		driver.quit();
 	}
 	
 	public WebDriverWait getWebDriverWait(){
